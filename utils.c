c82377c7ef73d65aa824aff64e9448a4e5e0d936
Kaio Victor Alves da Rocha <kaio@MacBook-Air-de-Kaio.local>
2025-01-06 10:15:08 -0300

    desenvolvendo a funçao gerar_bpm

--- utils.c
+++ utils.c
@@ -34,7 +34,7 @@ bool validar_id(const char *id){
 }
 
 //gerar a sequencia de 0s e 1s
-void gerar_sequencia_binaria(const char *id, char *barras){
+void gerar_sequencia_binaria(const char *id){
     
     //tabela l code
     const char *L_code[] = {"0001101", "0011001", "0010011", "0111101", "0100011",
@@ -43,23 +43,46 @@ void gerar_sequencia_binaria(const char *id, char *barras){
     const char *R_code[] = {"1110010", "1100110", "1101100", "1000010", "1011100",
                             "1001110", "1010000", "1000100", "1001000", "1110100"};
 
-    //marcador inicial
-    strcpy(barras, "101");
 
-    // 4 primeiros digitos
-    for(int i = 0; i < 4; i++){
+    // Calcula o tamanho
+    int tamanho = 3 + 4 * 7 + 5 + 4 * 7 + 3 + 1;
+
+    // Aloca dinamicamente a memória
+    char barras = (char)malloc(tamanho * sizeof(char));
+    if (!barras) {
+        fprintf(stderr, "Erro: Falha na alocação de memória.\n");
+        exit(1);
+    }
+
+    // Preenche a sequência binária
+    strcpy(barras, "101"); // Marcador inicial
+    for (int i = 0; i < 4; i++) {
         strcat(barras, L_code[id[i] - '0']);
     }
-
-    //marcador central
-    strcat(barras, "01010");
-
-    //4 ultimos digitos
-    for(int i = 4; i < 8; i++){
+    strcat(barras, "01010"); // Marcador central
+    for (int i = 4; i < 8; i++) {
         strcat(barras, R_code[id[i] - '0']);
     }
+    strcat(barras, "101"); // Marcador final
 
-    //marcador final
-    strcat(barras, "101");
+    return barras; // Retorna a sequência binária
+}
 
+void gerar_bpm(const char *sequencia_binaria, int larguraModulo, int altura, int espacamentoLateral, const char *nomeArquivo){
+    int larguraCodigo = strlen(sequencia_binaria) * larguraModulo;
+    int larguraTotal = larguraCodigo + 2 * espacamentoLateral;
+    int alturaTotal = altura + 2 * espacamentoLateral;
+
+    //alocar memoria para a matriz
+    int **matriz = (int**)malloc(alturaTotal * sizeof(int*));
+    for (int i = 0; i < alturaTotal; i++){
+        matriz[i] = (int*)malloc(larguraTotal * sizeof(int));
+    }
+
+    //preencher toda a matriz com 0s
+    for(int i = 0; i < alturaTotal; i++){
+        for(int j = 0; j < larguraTotal; j++){
+            matriz[i][j] = 0;        
+            }
+    }    
 }
